27015a7ea7d5b5d2030991b2b07927747c4c04cc
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-01-13 13:45:54 -0500

    adds handling of curl errors

--- http-security-check.sh
+++ http-security-check.sh
@@ -2,14 +2,7 @@
 #
 # Script to detect presence of HTTP security headers for a web application
 #
-# Usage: http-header-check.sh [OPTIONS] [https:]host[:port][/url]
-# Valid Options:
-#  -b              brief output, prints the test result in one line
-#  -s <header>     check for a single header
-#  -n              remove colorized output
-#  -v              verbose output, print the whole HTTP protocol response
-#                  without the content
-#  -t <seconds>    set the timeout for curl, default is 5 seconds
+# See usage function for details regarding options and arguments.
 
 usage() {
     echo "Usage: http-header-check.sh [OPTIONS] [https:]host[:port][/url]"
@@ -24,7 +17,6 @@ usage() {
     echo "                  cert requires it, a prompt will appear"
     echo "  -t <seconds>    set the timeout for curl, default is 5 seconds"
     echo "  -k              check all cookies for security flags (secure, httponly)"
-    exit 1
 }
 
 #######################################
@@ -42,47 +34,52 @@ usage() {
 print_output() {
     echo "#################################################"
     echo -e "$neu URL: $url"
-    echo -e "$neu HTTP Status Code: $http_status_code"
+    if [ "${#errors}" -gt 0 ]; then
+        echo -e "$neg Errors encountered:"
+        echo -e "$errors"
+    else
+        echo -e "$neu HTTP Status Code: $http_status_code"
     
-    if $verbose; then
-        echo "HTTP Response Headers:"
-        echo "$response" | sed 's/^M$//'
-    fi
+        if $verbose; then
+            echo "HTTP Response Headers:"
+            echo "$response" | sed 's/^M$//'
+        fi
 
-    echo -e "$neu HTTP Header Status:"
-    if [ ${#csp} -gt 0 ]; then
-        echo -e "$pos Content-Security-Policy header found"
-    else
-        echo -e "$neg Content-Security-Policy header missing"
-    fi
+        echo -e "$neu HTTP Header Status:"
+        if [ ${#csp} -gt 0 ]; then
+            echo -e "$pos Content-Security-Policy header found"
+        else
+            echo -e "$neg Content-Security-Policy header missing"
+        fi
 
-    if [ ${#sts} -gt 0 ]; then
-        echo -e "$pos Strict-Transport-Security header found"
-    else
-        echo -e "$neg Strict-Transport-Security header missing"
-    fi
+        if [ ${#sts} -gt 0 ]; then
+            echo -e "$pos Strict-Transport-Security header found"
+        else
+            echo -e "$neg Strict-Transport-Security header missing"
+        fi
 
-    if [ ${#xco} -gt 0 ]; then
-        echo -e "$pos X-Content-Type-Options header found"
-    else
-        echo -e "$neg X-Content-Type-Options header missing"
-    fi
+        if [ ${#xco} -gt 0 ]; then
+            echo -e "$pos X-Content-Type-Options header found"
+        else
+            echo -e "$neg X-Content-Type-Options header missing"
+        fi
 
-    if [ ${#xfr} -gt 0 ]; then
-        echo -e "$pos X-Frame-Options header found"
-    else
-        echo -e "$neg X-Frame-Options header missing"
-    fi
+        if [ ${#xfr} -gt 0 ]; then
+            echo -e "$pos X-Frame-Options header found"
+        else
+            echo -e "$neg X-Frame-Options header missing"
+        fi
 
-    if [ ${#xss} -gt 0 ]; then
-        echo -e "$pos X-XSS-Protection header found"
-    else
-        echo -e "$neg X-XSS-Protection header missing"
-    fi
+        if [ ${#xss} -gt 0 ]; then
+            echo -e "$pos X-XSS-Protection header found"
+        else
+            echo -e "$neg X-XSS-Protection header missing"
+        fi
 
-    if [ ${#insecure_cookies} -gt 0 ]; then
-        echo -e "Insecure Cookies:"
-        echo -e "$insecure_cookies"
+        if [ ${#insecure_cookies} -gt 0 ]; then
+            echo -e "Insecure Cookies:"
+            echo -e "$insecure_cookies"
+        fi
     fi
 
     echo "#################################################"
@@ -101,21 +98,26 @@ print_output() {
 #   None
 #######################################
 print_output_brief() {
-    if [ ${#csp} -gt 0 ]; then csp_bool=true; else csp_bool=false; fi
-    if [ ${#sts} -gt 0 ]; then sts_bool=true; else sts_bool=false; fi
-    if [ ${#xco} -gt 0 ]; then xco_bool=true; else xco_bool=false; fi
-    if [ ${#xfr} -gt 0 ]; then xfr_bool=true; else xfr_bool=false; fi
-    if [ ${#xss} -gt 0 ]; then xss_bool=true; else xss_bool=false; fi
+    if [ "${#errors}" -gt 0 ]; then
+        errors=$(echo "$errors" | tr '\n' '; ')
+        output_string="$url errors: $errors"
+    else
+        if [ ${#csp} -gt 0 ]; then csp_bool=true; else csp_bool=false; fi
+        if [ ${#sts} -gt 0 ]; then sts_bool=true; else sts_bool=false; fi
+        if [ ${#xco} -gt 0 ]; then xco_bool=true; else xco_bool=false; fi
+        if [ ${#xfr} -gt 0 ]; then xfr_bool=true; else xfr_bool=false; fi
+        if [ ${#xss} -gt 0 ]; then xss_bool=true; else xss_bool=false; fi
 
-    insecure_cookies_brief=""
-    if [ ${#insecure_cookies} -gt 0 ]; then
-        insecure_cookies_brief=$(echo "$insecure_cookies" | tr '\n' '; ')
-    fi
+        insecure_cookies_brief=""
+        if [ ${#insecure_cookies} -gt 0 ]; then
+            insecure_cookies_brief=$(echo "$insecure_cookies" | tr '\n' '; ')
+        fi
 
-    output_string="$url $http_status_code CSP:$csp_bool STS:$sts_bool XCO:$xco_bool XFR:$xfr_bool XSS:$xss_bool"
+        output_string="$url $http_status_code CSP:$csp_bool STS:$sts_bool XCO:$xco_bool XFR:$xfr_bool XSS:$xss_bool"
 
-    if [ ${#insecure_cookies_brief} -gt 0 ]; then
-        output_string+=" Insecure Cookies: $insecure_cookies_brief"
+        if [ ${#insecure_cookies_brief} -gt 0 ]; then
+            output_string+=" Insecure Cookies: $insecure_cookies_brief"
+        fi
     fi
 
     printf "%s\n" "$output_string"
@@ -201,7 +203,15 @@ else
 fi
 
 curl="/usr/bin/curl"
-response=$($curl $certificate_option --connect-timeout $timeout -k -I $url 2>/dev/null)
+response=$($curl $certificate_option -sS --connect-timeout $timeout -k -I $url 2>&1)
+curl_exit_code="$?"
+
+# curl error handling
+errors=""
+if [ "$curl_exit_code" -ne "0" ]; then
+    errors=$(echo "$response" | cut -d ":" -f 2- | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//')
+fi
+
 http_status_code=$(echo "$response" | grep "HTTP/" | cut -d " " -f 2)
 
 # check for the HTTP security headers
@@ -214,7 +224,6 @@ xco=$(echo "$response" | grep -i 'x-content-type-options')
 # check for cookie security flags if desired
 insecure_cookies=""
 if $cookie_check; then
-    
     insecure_cookies=$(echo "$response" | grep -i 'Set-Cookie' | \
         grep -Eiv '(;\s*secure;\s*httponly\s*$|;\s*httponly;\s*secure\s*$)')
 fi
